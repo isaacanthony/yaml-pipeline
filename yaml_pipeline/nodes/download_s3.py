3a51dcedea77624ae74de0f8aaa2dcd0453bcb5c
Isaac <isaac@foundry.ai>
2020-05-11 16:50:17 -0400

    Add s3_prefix and local_prefix optional params.

--- yaml_pipeline/nodes/download_s3.py
+++ yaml_pipeline/nodes/download_s3.py
@@ -12,10 +12,21 @@ def run(dfs: dict, settings: dict) -> dict:
         if col not in settings:
             raise Exception(f"Missing {col} param")
 
+    # Add prefixes
+    if 's3_prefix' in settings:
+        remote_path = settings['s3_prefix'] + settings['remote_path']
+    else:
+        remote_path = settings['remote_path']
+
+    if 'local_prefix' in settings:
+        local_path = settings['local_prefix'] = settings['local_path']
+    else:
+        local_path = settings['local_path']
+
     # Skip downloading if flag set
     skip_if_exists = settings['skip_if_exists'] if 'skip_if_exists' in settings else False
 
-    if skip_if_exists and path.exists(settings['local_path']):
+    if skip_if_exists and path.exists(local_path):
         return dfs
 
     # Set credentials if exist
@@ -23,7 +34,5 @@ def run(dfs: dict, settings: dict) -> dict:
     secret = settings['aws_secret_access_key'] if 'aws_secret_access_key' in settings else None
 
     # Download
-    S3FileSystem(key=key, secret=secret) \
-        .get(settings['remote_path'], settings['local_path'])
-
+    S3FileSystem(key=key, secret=secret).get(remote_path, local_path)
     return dfs
