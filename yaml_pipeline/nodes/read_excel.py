8ff6dad493e0aa2ab49226c74121028caed4b3db
Isaac <isaac@foundry.ai>
2020-05-12 10:20:51 -0400

    Add path prefix to read_excel.

--- yaml_pipeline/nodes/read_excel.py
+++ yaml_pipeline/nodes/read_excel.py
@@ -7,11 +7,12 @@ def run(dfs: dict, settings: dict) -> dict:
     if 'path' not in settings:
         raise Exception('Missing path param')
 
+    prefix = settings['local_path_prefix'] if 'local_path_prefix' in settings else ''
     sheet_name = settings['sheet_name'] if 'sheet_name' in settings else 0
     skiprows = settings['skiprows'] if 'skiprows' in settings else None
 
     dfs[settings['df']] = pd.read_excel(
-        settings['path'],
+        prefix + settings['path'],
         sheet_name=sheet_name,
         skiprows=skiprows,
     )
