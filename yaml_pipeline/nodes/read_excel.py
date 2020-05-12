41a0410c7698ce474afb9f50f12b6ab9d0c5bed8
Isaac <isaac@foundry.ai>
2020-05-12 10:15:21 -0400

    Make read_excel sheet_name param optional.

--- yaml_pipeline/nodes/read_excel.py
+++ yaml_pipeline/nodes/read_excel.py
@@ -4,15 +4,15 @@ import pandas as pd
 
 def run(dfs: dict, settings: dict) -> dict:
     """pd.read_excel()"""
-    for setting in ['path', 'sheet_name']:
-        if setting not in settings:
-            raise Exception(f"Missing {setting} param")
+    if 'path' not in settings:
+        raise Exception('Missing path param')
 
+    sheet_name = settings['sheet_name'] if 'sheet_name' in settings else 0
     skiprows = settings['skiprows'] if 'skiprows' in settings else None
 
     dfs[settings['df']] = pd.read_excel(
         settings['path'],
-        sheet_name=settings['sheet_name'],
+        sheet_name=sheet_name,
         skiprows=skiprows,
     )
 
